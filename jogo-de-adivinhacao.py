a63010249c80a7deeb5f2adc000c1d198a4da711
EduOrsi <eduardo.fexlim@gmail.com>
2022-07-31 17:24:46 -0300

    implantação do numero de tentativas e melhorias no codigo
    implantação do numero de tentativas e melhorias no codigo

--- jogo-de-adivinhacao.py
+++ jogo-de-adivinhacao.py
@@ -2,12 +2,27 @@ print("#########################")
 print("###JOGO DE ADIVINHAÇÂO###")
 print("#########################")
 
-number = 10
+riddle = 10
 attempts = 3
+turn = 1
 
-kick = int(input("Digite um número: "))
 
-if(kick == number):
-    print("Parabéns! Você advinhou o número.")
-else:
-    print(f"Você errou, o número era {number} .Tente novamente.")
+while(turn <= attempts):
+    print(f"-Tentativa {turn} de {attempts}")
+    kick = int(input("Digite um número: "))
+    accurate = kick == riddle
+    bigger = kick > riddle
+
+
+    if(accurate):
+        print("Parabéns! Você advinhou o número.\n")
+        break
+    else:
+        if(bigger):
+            print(f"Você errou. {kick} é maior que o número a ser adivinhado.\n")
+        else:
+            print(f"Você errou. {kick} é menor que o número a ser adivinhado.\n")
+
+    turn = turn + 1
+
+print("Fim de jogo.")
